6aca94888768dc2bc7b3c374ad33eab4b28b7235
osawa-koki <github@osawa.dev>
2023-02-20 15:39:35 +0900

    変数。

--- src/main.pas
+++ src/main.pas
@@ -1,4 +1,12 @@
 program HelloWorld;
+var
+  num1, num2, sum : integer;
 begin
   writeln('Hello, World!');
+
+  // 変数に値を代入
+  num1 := 5;
+  num2 := 10;
+  sum := num1 + num2;
+  writeln('The sum of ', num1, ' and ', num2, ' is ', sum);
 end.
