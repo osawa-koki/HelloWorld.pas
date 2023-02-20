19419048d9e69f0c21427e6fd5346a8a3f51c798
osawa-koki <github@osawa.dev>
2023-02-20 15:44:48 +0900

    標準入力の受け取り・条件分岐・ループ

--- src/main.pas
+++ src/main.pas
@@ -1,6 +1,7 @@
 program Main;
 var
   num1, num2, sum : integer;
+  i, n : integer;
 begin
   writeln('Hello, World!');
 
@@ -9,4 +10,22 @@ begin
   num2 := 10;
   sum := num1 + num2;
   writeln('The sum of ', num1, ' and ', num2, ' is ', sum);
+
+  // 入力の受け取り
+  writeln('Enter a number: ');
+  readln(n);
+
+  // 入力された値を出力
+  writeln('You entered ', n);
+
+  // 偶数か奇数かを判定
+  if n mod 2 = 0 then
+    writeln('The number is even.')
+  else
+    writeln('The number is odd.');
+
+  // ループ
+  for i := 1 to n do
+    writeln(i);
+
 end.
